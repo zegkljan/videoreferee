57cc6137b582e50bccac511c2336b0339f7ff077
Jan Žegklitz <zegkljan@gmail.com>
2021-09-18 17:00:40 +0200

    Permissions check put to correct place to avoid crash when permissions were already granted.

--- app/src/main/java/cz/zegkljan/videoreferee/fragments/PermissionsFragment.kt
+++ app/src/main/java/cz/zegkljan/videoreferee/fragments/PermissionsFragment.kt
@@ -20,6 +20,9 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -36,17 +39,21 @@ private val PERMISSIONS_REQUIRED = arrayOf(
  */
 class PermissionsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         if (hasPermissions(requireContext())) {
             // If permissions have already been granted, proceed
-            Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                    PermissionsFragmentDirections.actionPermissionsToSelector())
+            val activity = requireActivity()
+            val navController = Navigation.findNavController(activity, R.id.fragment_container)
+            navController.navigate(PermissionsFragmentDirections.actionPermissionsToSelector())
         } else {
             // Request camera-related permissions
             requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
         }
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onRequestPermissionsResult(
@@ -64,7 +71,6 @@ class PermissionsFragment : Fragment() {
     }
 
     companion object {
-
         /** Convenience method used to check if all permissions required by this app are granted */
         fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
             ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
